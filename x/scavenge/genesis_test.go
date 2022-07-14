7e69b01544f47910919809f45732cc480ebaec93
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2022-07-14 10:57:05 +0100

    scaffold scavenge chain and module

---
+++ x/scavenge/genesis_test.go
@@ -0,0 +1,29 @@
+package scavenge_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	keepertest "github.com/uwezukwechibuzor/scavenge/testutil/keeper"
+	"github.com/uwezukwechibuzor/scavenge/testutil/nullify"
+	"github.com/uwezukwechibuzor/scavenge/x/scavenge"
+	"github.com/uwezukwechibuzor/scavenge/x/scavenge/types"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.ScavengeKeeper(t)
+	scavenge.InitGenesis(ctx, *k, genesisState)
+	got := scavenge.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
---
+++ x/scavenge/keeper/grpc_query.go
@@ -0,0 +1,7 @@
+package keeper
+
+import (
+	"github.com/uwezukwechibuzor/scavenge/x/scavenge/types"
+)
+
+var _ types.QueryServer = Keeper{}
